e7e51dc21b07d17def06a44a3c80e1e8fbf04ceb
Martin Hesko <martinhesko8@gmail.com>
2020-07-03 12:21:57 +0200

    added health checks and fixed zombie process issues

--- benchmarks/mongo10types.js
+++ benchmarks/mongo10types.js
@@ -1,18 +1,13 @@
 const fs = require('fs');
 const { genSchema } = require("../lib/genSchema.js");
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 fs.writeFile("templates/ts-apollo-mongodb-backend/model/Note.graphql", genSchema(5), function(err) {
     if(err) {
         return console.log(err);
     }
 });
-exec('lib/mongoprep.sh',
-        (error, stdout, stderr) => {
-            console.log(stdout);
-            console.log(stderr);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
+const server = spawn('lib/mongoprep.sh')
+process.on('SIGINT', () => {server.kill()})
+
 
 
